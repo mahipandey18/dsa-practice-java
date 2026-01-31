28993ac6a5498280ddad073ca9ca6458213b0b74
Mahi Pandey <mahipandey1813@gmail.com>
2026-02-01 02:02:00 +0530

    Update leetcode_1920.java

--- LeetCode/Arrays/leetcode_1920.java
+++ LeetCode/Arrays/leetcode_1920.java
@@ -1,1 +1,10 @@
 // Solution
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int arr[] = new int[nums.length];
+        for(int i = 0; i < nums.length; i++){
+            arr[i] = nums[nums[i]];
+        }
+        return arr;
+    }
+}
