8bfe38f7abbf8a5ab91448b196e5bb87d36dac9e
Mahi Pandey <mahipandey1813@gmail.com>
2026-02-01 23:26:08 +0530

    Update leetcode_0053.java

--- LeetCode/Arrays/leetcode_0053.java
+++ LeetCode/Arrays/leetcode_0053.java
@@ -1,1 +1,15 @@
 // Solution
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int res = nums[0];
+        int totalSum = 0;
+        for(int n : nums){
+            if(totalSum < 0){
+                totalSum = 0;
+            }
+            totalSum +=n;
+            res = Math.max(res,totalSum);
+        }
+        return res;
+    }
+}
