6a173e7127bed5be13f2ce1175088b3cc5f9be05
Mahi Pandey <mahipandey1813@gmail.com>
2026-02-02 23:18:09 +0530

    Update leetcode_0169.java

--- LeetCode/Arrays/leetcode_0169.java
+++ LeetCode/Arrays/leetcode_0169.java
@@ -1,1 +1,19 @@
 // Solution
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int num = 0;
+        for(int curr : nums){
+            if(count == 0){
+                num = curr;
+            }
+            if(curr == num){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return num;
+    }
+}
