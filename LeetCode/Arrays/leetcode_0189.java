b956e0ad1dd2f6dfabc7ab83c48bb30b02534ac9
Mahi Pandey <mahipandey1813@gmail.com>
2026-02-03 19:11:25 +0530

    Update leetcode_0189.java

--- LeetCode/Arrays/leetcode_0189.java
+++ LeetCode/Arrays/leetcode_0189.java
@@ -1,2 +1,16 @@
 // Solution
+class Solution {
+    public void rotate(int[] nums, int k) {
+        //for rotated array - i index => (i+k)%length index
+        int n = nums.length;
+        int rotated[] = new int[n];
 
+        for(int i = 0; i<n; i++){
+            rotated[(i+k)%n] = nums[i];
+        }
+
+        for(int j = 0; j<n; j++){
+            nums[j] = rotated[j];
+        }
+    }
+}
