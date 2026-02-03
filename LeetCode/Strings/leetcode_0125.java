27e15791c5286748f687015cf526256d855a2f53
Mahi Pandey <mahipandey1813@gmail.com>
2026-02-03 22:07:13 +0530

    Update leetcode_0125.java

--- LeetCode/Strings/leetcode_0125.java
+++ LeetCode/Strings/leetcode_0125.java
@@ -1,1 +1,16 @@
 // Solution
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[',:\"\\s]", "");
+
+        s = s.toLowerCase();
+        int len = s.length();
+
+        for(int i = 0; i < len; i++){
+            if(s.charAt(i) != s.charAt(len-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
