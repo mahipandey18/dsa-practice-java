f24d223365e66f4b47aa8397efb252073264bac3
Mahi Pandey <mahipandey1813@gmail.com>
2026-02-03 23:23:47 +0530

    Update leetcode_0344.java

--- LeetCode/Strings/leetcode_0344.java
+++ LeetCode/Strings/leetcode_0344.java
@@ -1,3 +1,9 @@
+// LeetCode 344: Reverse String
+// Approach: Use two pointers starting from the beginning and end of the array, swapping characters until they meet
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+// Learning: Two-pointer technique allows in-place reversal without extra memory
+
 // Solution
 class Solution {
     public void reverseString(char[] s) {
