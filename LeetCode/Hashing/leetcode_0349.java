cb7f14487967627188fd2599ef1d78a2c6bdea35
Mahi Pandey <mahipandey1813@gmail.com>
2026-02-05 22:25:10 +0530

    Update leetcode_0349.java

--- LeetCode/Hashing/leetcode_0349.java
+++ LeetCode/Hashing/leetcode_0349.java
@@ -1,1 +1,24 @@
 // Solution
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Map <Integer, Integer> map = new HashMap<>();
+        List <Integer> res = new ArrayList<>();
+
+        for(int x : nums1){
+            map.put(x,1);
+        }
+        for(int y : nums2){
+            if(map.containsKey(y) && map.get(y) == 1){
+                res.add(y);
+                map.put(y,0);
+            } 
+        }
+        int[] ans = new int[res.size()];
+        
+        for (int i = 0; i < res.size(); i++) {
+            ans[i] = res.get(i);
+        }
+
+        return ans;
+    }
+}
