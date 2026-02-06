764e1c6ac9b19d3a36f356a8a64de2fecc020325
Mahi Pandey <mahipandey1813@gmail.com>
2026-02-06 19:39:18 +0530

    Implement palindrome check using two pointers

--- LeetCode/Two-pointers/leetcode_125.java
+++ LeetCode/Two-pointers/leetcode_125.java
@@ -1,1 +1,20 @@
 // Solution
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]", "");
+        s = s.toLowerCase();
+        int len = s.length();
+
+        int left = 0;
+        int right = len-1;
+        
+        while(left < right && left < len && right >= 0){
+            if(s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
