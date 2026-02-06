c15b421e29eb3463ab8fc66e154248ed500843ad
Mahi Pandey <mahipandey1813@gmail.com>
2026-02-07 00:23:24 +0530

    Update leetcode_0015.java

--- LeetCode/Two-pointers/leetcode_0015.java
+++ LeetCode/Two-pointers/leetcode_0015.java
@@ -1,1 +1,35 @@
 // Solution
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        List <List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        
+        for(int i = 0; i<n; i++){
+            if(i>0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            int j = i+1;
+            int k = n-1;
+
+            while(j<k){
+                int total = nums[i]+nums[j]+nums[k];
+                if(total>0){
+                    k--;
+                }
+                else if(total<0){
+                    j++;
+                }
+                else{
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                    // Avoiding duplicate triplets - nums[j-1] is the recently added value to res.
+                    while (j < k && nums[j] == nums[j - 1]) j++;
+                    while (j < k && nums[k] == nums[k + 1]) k--;
+                }
+            }
+        }
+        return res;
+    }
+}
