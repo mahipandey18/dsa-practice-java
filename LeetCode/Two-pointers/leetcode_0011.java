9f6ecb898dfaff0429ddd1f9ce52142970a96231
Mahi Pandey <mahipandey1813@gmail.com>
2026-02-06 23:36:27 +0530

    Update leetcode_0011.java

--- LeetCode/Two-pointers/leetcode_0011.java
+++ LeetCode/Two-pointers/leetcode_0011.java
@@ -1,1 +1,21 @@
 // Solution
+class Solution {
+    public int maxArea(int[] height) {
+        int n = height.length;
+        int l = 0;
+        int r = n-1;
+        int area = 0;
+
+        while(l<r){
+            int h = Math.min(height[l], height[r]);
+            int len = r - l;
+            area = Math.max(area,h*len);
+            if (height[l] < height[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }      
+        return area;
+    }
+}
