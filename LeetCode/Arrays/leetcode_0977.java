5ef466a0b6e1f145acb681ffe85b2fe7b8a4246a
Mahi Pandey <mahipandey1813@gmail.com>
2026-02-06 20:20:09 +0530

    Update leetcode_0977.java

--- LeetCode/Arrays/leetcode_0977.java
+++ LeetCode/Arrays/leetcode_0977.java
@@ -1,1 +1,11 @@
 // Solution
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int res[] = new int[nums.length];
+        for(int i = 0; i<nums.length; i++){
+            res[i] = nums[i]*nums[i];
+        }
+        Arrays.sort(res);
+        return res;
+    }
+}
