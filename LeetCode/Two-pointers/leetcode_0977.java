d6a0dc640cf61545c08aae89a5e15fc5341df3cf
Mahi Pandey <mahipandey1813@gmail.com>
2026-02-06 20:46:05 +0530

    Update leetcode_0977.java

--- LeetCode/Two-pointers/leetcode_0977.java
+++ LeetCode/Two-pointers/leetcode_0977.java
@@ -1,4 +1,3 @@
-// Solution
 // LeetCode 977: Squares of a Sorted Array
 // Approach: Use two pointers at the beginning and end of the sorted array; compare squared values and place the larger square at the end of the result array, moving pointers inward
 // Time Complexity: O(n)
