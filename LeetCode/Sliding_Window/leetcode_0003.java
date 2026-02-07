4611ecfe30741c52396419a6201869130eb4d557
Mahi Pandey <mahipandey1813@gmail.com>
2026-02-07 19:52:28 +0530

    Update leetcode_0003.java

--- LeetCode/Sliding_Window/leetcode_0003.java
+++ LeetCode/Sliding_Window/leetcode_0003.java
@@ -1,1 +1,18 @@
 // Solution
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        HashSet<Character> c = new HashSet<>();
+        int n = s.length();
+        int i = 0;
+        int maxLen = 0;
+        for(int j = 0; j < n; j++){
+            while(c.contains(s.charAt(j))){
+                c.remove(s.charAt(i));
+                i++;
+            }
+            c.add(s.charAt(j));
+            maxLen = Math.max(maxLen, j-i+1);
+        }
+        return maxLen;
+    }
+}
