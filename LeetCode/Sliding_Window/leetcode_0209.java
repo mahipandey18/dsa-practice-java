6f266e0088edd665de85c7c1cdb317cdd6581d6f
Mahi Pandey <mahipandey1813@gmail.com>
2026-02-07 19:07:27 +0530

    Update leetcode_0209.java

--- LeetCode/Sliding_Window/leetcode_0209.java
+++ LeetCode/Sliding_Window/leetcode_0209.java
@@ -1,1 +1,22 @@
 // Solution
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int n = nums.length;
+        int i = 0;
+        int sum = 0;
+        int minLen = n+1;
+
+        for(int j = 0; j<n;j++){
+            sum += nums[j];
+            while(sum>=target){
+                minLen = Math.min(minLen, j-i+1);
+                sum-=nums[i];
+                i++;
+            }
+        }
+        if(minLen == n+1){
+            return 0;
+        }
+        return minLen;
+    }
+}
