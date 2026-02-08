04c35ba1c0b67c6df70894f5e7ed5bc9fa9209ab
Mahi Pandey <mahipandey1813@gmail.com>
2026-02-08 23:21:22 +0530

    Update leetcode_0020.java

--- LeetCode/Stacks/leetcode_0020.java
+++ LeetCode/Stacks/leetcode_0020.java
@@ -1,1 +1,25 @@
 // Solution
+class Solution {
+    public boolean isValid(String s) {
+        Stack <Character> st = new Stack<>();
+        int n = s.length();
+        
+        for(int i = 0; i<n; i++){
+            if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='['){
+                st.push(s.charAt(i));
+            }
+            else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                if(s.charAt(i)==')' && st.peek()=='(' || s.charAt(i)=='}' && st.peek()=='{' || s.charAt(i)==']' && st.peek()=='['){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+}
