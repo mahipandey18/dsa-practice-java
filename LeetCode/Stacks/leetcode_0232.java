c37fbeed071e0ca0d92691704605e9c78e71cfd9
Mahi Pandey <mahipandey1813@gmail.com>
2026-02-09 23:26:04 +0530

    Update leetcode_0232.java

--- LeetCode/Stacks/leetcode_0232.java
+++ LeetCode/Stacks/leetcode_0232.java
@@ -5,3 +5,43 @@
 // Learning: Using two stacks with lazy transfer simulates queue behavior while keeping operations efficient
 
 // Solution
+class MyQueue {
+    Stack <Integer> inQ;
+    Stack <Integer> outQ;
+
+    public MyQueue() {
+        inQ = new Stack<>();
+        outQ = new Stack<>();
+    }
+    
+    public void push(int x) {
+        inQ.push(x);
+    }
+    
+    public int pop() {
+        peek();
+        return outQ.pop();        
+    }
+    
+    public int peek() {
+        if(outQ.isEmpty()){
+            while(!inQ.isEmpty()){
+                outQ.push(inQ.pop());
+            }
+        }
+        return outQ.peek();
+    }
+    
+    public boolean empty() {
+        return outQ.isEmpty() && inQ.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
