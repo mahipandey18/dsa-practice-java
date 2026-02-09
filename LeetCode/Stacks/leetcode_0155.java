ca4c17c138a9f02cff13d6773d806df4c7163a2a
Mahi Pandey <mahipandey1813@gmail.com>
2026-02-09 23:01:41 +0530

    Update leetcode_0155.java

--- LeetCode/Stacks/leetcode_0155.java
+++ LeetCode/Stacks/leetcode_0155.java
@@ -1,1 +1,52 @@
 // Solution
+class MinStack {
+    Stack <Long> st;
+    long min;
+
+    public MinStack() {
+        st = new Stack<>();
+    }
+    
+    public void push(int val) {
+        if(st.isEmpty()){
+            st.push(0L);
+            min = val;
+        }
+        else{
+            st.push(val-min);
+            if(val<min){
+                min = val;
+            }
+        }
+    }
+    
+    public void pop() {
+        long top = st.pop();
+        if(top<0){
+            min = min - top;
+        }
+    }
+    
+    public int top() {
+        long top = st.peek();
+        if(top < 0){
+            return (int)min;
+        }
+        else{
+            return (int)(min+top);
+        }
+    }
+    
+    public int getMin() {
+        return (int)min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
