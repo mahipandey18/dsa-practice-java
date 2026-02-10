940d3d72528c4badef4e288804d4b6b5ac158416
Mahi Pandey <mahipandey1813@gmail.com>
2026-02-10 23:29:40 +0530

    Update leetcode_0503.java

--- LeetCode/Stacks/leetcode_0503.java
+++ LeetCode/Stacks/leetcode_0503.java
@@ -5,3 +5,28 @@
 // Learning: Pre-filling the stack can simulate circular array behavior, but using a simple Stack with repeated pops for duplicates may lead to inefficiency; understanding stack usage for "next greater" problems is key
 
 // Solution
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        Stack <Integer> st = new Stack<>();
+        int n = nums.length;
+        int res[] = new int[n];
+
+        for(int i = n - 1 ; i >= 0 ; i--){
+            st.push(nums[i]);
+        }
+
+        for(int i = n - 1 ; i >= 0 ; i--){
+            while(!st.isEmpty() && st.peek()<=nums[i]){
+                st.pop();
+            }
+             if(!st.isEmpty()){
+                res[i] = st.peek();
+            }
+            else{
+                res[i] = -1;
+            }
+            st.push(nums[i]);
+        }
+        return res;
+    }
+}
