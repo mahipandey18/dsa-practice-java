b74997f4e94120129996d486ee550015cc8f9f0e
Mahi Pandey <mahipandey1813@gmail.com>
2026-02-12 20:36:08 +0530

    Update leetcode_0084.java

--- LeetCode/Stacks/leetcode_0084.java
+++ LeetCode/Stacks/leetcode_0084.java
@@ -1,1 +1,34 @@
 // Solution
+import java.util.*;
+
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        Stack <Integer> st = new Stack<>();
+        int maxArea = 0;
+        int currH = 0;
+        int n = heights.length;
+
+        for(int i = 0; i<=n; i++){
+            if(i==n){
+                currH = 0;
+            }
+            else{
+                currH = heights[i];
+            }
+
+            while(!st.isEmpty() && currH < heights[st.peek()]){
+                int h = heights[st.pop()];
+                int w;
+                if(st.isEmpty()){
+                    w = i;
+                }
+                else{
+                    w = i-st.peek()-1;
+                }
+                maxArea = Math.max(maxArea, h*w);
+            }
+            st.push(i);
+        }
+        return maxArea;
+    }
+}
