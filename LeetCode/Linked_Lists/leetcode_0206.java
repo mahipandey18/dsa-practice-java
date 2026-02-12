9562cf603bc246e24620811ea6a90b1509decc17
Mahi Pandey <mahipandey1813@gmail.com>
2026-02-13 00:18:18 +0530

    Update leetcode_0206.java

--- LeetCode/Linked_Lists/leetcode_0206.java
+++ LeetCode/Linked_Lists/leetcode_0206.java
@@ -1,1 +1,25 @@
 // Solution
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode temp = head;
+        ListNode prev = null;
+
+        while(temp != null){
+            ListNode newNode = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = newNode;
+        }  
+        return prev;      
+    }
+}
