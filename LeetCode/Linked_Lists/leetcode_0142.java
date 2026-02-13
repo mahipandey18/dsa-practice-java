bbf02354a682fcf794e89d64f77b36275ad2890b
Mahi Pandey <mahipandey1813@gmail.com>
2026-02-13 23:29:43 +0530

    Update leetcode_0142.java

--- LeetCode/Linked_Lists/leetcode_0142.java
+++ LeetCode/Linked_Lists/leetcode_0142.java
@@ -5,3 +5,36 @@
 // Learning: After detecting a cycle, moving one pointer to head and advancing both at same speed guarantees they meet at the cycle entry
 
 // Solution
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow==fast){
+                slow = head;
+                while(slow!=fast){
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
