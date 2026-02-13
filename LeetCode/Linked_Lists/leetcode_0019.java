625ed545167408c134be881bb82757bc9076092e
Mahi Pandey <mahipandey1813@gmail.com>
2026-02-14 01:05:49 +0530

    Update leetcode_0019.java

--- LeetCode/Linked_Lists/leetcode_0019.java
+++ LeetCode/Linked_Lists/leetcode_0019.java
@@ -5,3 +5,37 @@
 // Learning: Two-pass approach is simple; careful handling of edge cases like removing the head is important
 
 // Solution
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode temp = head;
+        int count = 0;
+
+        while(temp!=null){
+            count++;
+            temp = temp.next;
+        }
+
+        if (n == count) {
+            return head.next;
+        }
+
+        temp = head;
+        for(int i = 1; i<count-n; i++){
+            temp = temp.next;
+        }
+        temp.next = temp.next.next;
+        
+        return head;
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
