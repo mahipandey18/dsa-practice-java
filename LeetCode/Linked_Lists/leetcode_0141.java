3b437ddbf6b5c2e73d04be45ccddbbb49905fd8b
Mahi Pandey <mahipandey1813@gmail.com>
2026-02-13 20:32:23 +0530

    Update leetcode_0141.java

--- LeetCode/Linked_Lists/leetcode_0141.java
+++ LeetCode/Linked_Lists/leetcode_0141.java
@@ -1,1 +1,31 @@
+// Solution
+// 1. By Hashing
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        Set <ListNode> set = new HashSet<>();
+        ListNode temp = head;
 
+        if (head==null)return false;
+
+        while(temp!=null){
+            if(set.contains(temp)){
+                return true;
+            }
+            set.add(temp);
+            temp = temp.next;
+        }
+        return false;
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
