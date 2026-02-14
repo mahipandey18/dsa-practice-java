313c95fc2214693a2b6bc99037a295c079e132c1
Mahi Pandey <mahipandey1813@gmail.com>
2026-02-14 19:04:17 +0530

    Update leetcode_0035.java

--- LeetCode/Binary_Search/leetcode_0035.java
+++ LeetCode/Binary_Search/leetcode_0035.java
@@ -5,3 +5,25 @@
 // Learning: Binary search can be extended to determine the correct insertion position by carefully handling boundary updates when the target is not present
 
 // Solution
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        int low = 0;
+        int high = n-1;
+        int mid = 0;
+
+        while(low<=high){
+            mid = low+(high-low)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return mid+1;
+    }
+}
