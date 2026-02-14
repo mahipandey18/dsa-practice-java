d0e80b307ff6d7fb8bb2600afad27c6852e0a1ca
Mahi Pandey <mahipandey1813@gmail.com>
2026-02-14 20:33:55 +0530

    Update leetcode_0153.java

--- LeetCode/Arrays/leetcode_0153.java
+++ LeetCode/Arrays/leetcode_0153.java
@@ -1,1 +1,10 @@
 // Solution
+class Solution {
+    public int findMin(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        for(int num:nums){
+            min = Math.min(min,num);
+        }
+        return min;
+    }
+}
