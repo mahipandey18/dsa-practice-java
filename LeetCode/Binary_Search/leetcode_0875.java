0e384070c996d9161f5bd24e204a96a17f5de589
Mahi Pandey <mahipandey1813@gmail.com>
2026-02-16 22:32:05 +0530

    Update leetcode_0875.java

--- LeetCode/Binary_Search/leetcode_0875.java
+++ LeetCode/Binary_Search/leetcode_0875.java
@@ -5,3 +5,32 @@
 // Learning: Binary search on answer works when the solution space is monotonic, and careful handling of ceiling division and overflow (using long) ensures correctness for large inputs
 
 // Solution
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int low = 1;
+        int high = 0;
+
+        for(int pile : piles){
+            high = Math.max(high,pile);
+        }
+
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(inTime(piles,h,mid)){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return low;
+    }
+
+    private boolean inTime(int[]piles, int h, int speed){
+        long hours = 0;
+        for(int pile : piles){
+            hours += (pile + speed - 1)/speed;
+        }
+        return hours <= h;
+    }
+}
