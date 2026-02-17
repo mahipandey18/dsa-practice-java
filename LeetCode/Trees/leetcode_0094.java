22199d3511d4534a4563f2a51e27d814ae26e44d
Mahi Pandey <mahipandey1813@gmail.com>
2026-02-17 11:49:46 +0530

    Update leetcode_0094.java

--- LeetCode/Trees/leetcode_0094.java
+++ LeetCode/Trees/leetcode_0094.java
@@ -5,3 +5,35 @@
 // Learning: Inorder traversal follows Left-Root-Right order and is especially important for Binary Search Trees because it returns elements in sorted order
 
 // Solution
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+
+        inorder(root,res);
+        return res;
+    }
+    private void inorder(TreeNode root, List<Integer> res){
+        if(root == null){
+            return;
+        }
+        inorder(root.left,res);
+        res.add(root.val);
+        inorder(root.right,res);
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
