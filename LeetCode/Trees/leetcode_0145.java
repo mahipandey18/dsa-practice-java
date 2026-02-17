2472503f76a8f3aa4201fc433eee61242d860ff9
Mahi Pandey <mahipandey1813@gmail.com>
2026-02-17 11:53:49 +0530

    Update leetcode_0145.java

--- LeetCode/Trees/leetcode_0145.java
+++ LeetCode/Trees/leetcode_0145.java
@@ -5,3 +5,34 @@
 // Learning: Postorder traversal follows Left-Right-Root order and is useful for problems that require processing children before their parent, such as tree deletion or bottom-up calculations
 
 // Solution
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        postorder(root,res);
+        return res;
+    }
+    private void postorder(TreeNode root, List <Integer> res){
+        if(root==null){
+            return;
+        }
+        postorder(root.left,res);
+        postorder(root.right,res);
+        res.add(root.val);
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
