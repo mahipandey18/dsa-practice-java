7018ac9f3617f829987306bbb2759060d2dcdaf5
Mahi Pandey <mahipandey1813@gmail.com>
2026-02-17 11:42:24 +0530

    Update leetcode_0144.java

--- LeetCode/Trees/leetcode_0144.java
+++ LeetCode/Trees/leetcode_0144.java
@@ -5,3 +5,34 @@
 // Learning: Preorder traversal follows Root-Left-Right order and demonstrates how recursion naturally explores tree depth while using the call stack to manage traversal
 
 // Solution
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        preorder(root,res);
+        return res;
+    }
+
+    private void preorder(TreeNode root, List<Integer> res){
+        if(root==null){
+            return;
+        }
+        res.add(root.val);
+        preorder(root.left,res);
+        preorder(root.right,res);
+    }
+}
