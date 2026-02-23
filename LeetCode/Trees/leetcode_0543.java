e31f881fbfb263af3c909a405e5040b44c8128ee
Mahi Pandey <mahipandey1813@gmail.com>
2026-02-23 19:39:16 +0530

    Update leetcode_0543.java

--- LeetCode/Trees/leetcode_0543.java
+++ LeetCode/Trees/leetcode_0543.java
@@ -5,3 +5,37 @@
 // Learning: Many tree problems combine height calculation with a global answer update during postorder traversal, allowing diameter to be computed efficiently in a single DFS
 
 // Solution
+class Solution {
+    int maxLen;
+    public int diameterOfBinaryTree(TreeNode root) {
+        dia(root);
+        return maxLen;
+    }
+
+    private int dia(TreeNode node){
+        if(node==null){
+            return 0;
+        }
+
+        int l = dia(node.left);
+        int r = dia(node.right);
+        maxLen = Math.max(maxLen, l+r);
+
+        return 1+Math.max(l,r);
+    }
+}
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
