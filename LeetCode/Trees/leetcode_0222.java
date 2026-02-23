c024b304e54f573adefa206c8a7ec0bd550f2824
Mahi Pandey <mahipandey1813@gmail.com>
2026-02-23 20:10:28 +0530

    Update leetcode_0222.java

--- LeetCode/Trees/leetcode_0222.java
+++ LeetCode/Trees/leetcode_0222.java
@@ -5,3 +5,44 @@
 // Learning: Level-order traversal ensures each node is visited exactly once, making BFS a straightforward way to count all nodes in a binary tree
 
 // Solution
+class Solution {
+    public int countNodes(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        int count = 0;
+        Queue <TreeNode> q = new LinkedList<>();
+
+        q.add(root);
+        while(!q.isEmpty()){
+            TreeNode node = q.remove();
+            count++;
+
+            if(node.left!=null){
+                q.add(node.left);
+            }
+            if(node.right!=null){
+                q.add(node.right);
+            }
+
+        }
+
+        return count;
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
