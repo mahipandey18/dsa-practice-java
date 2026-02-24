146fd5988a7a625d721a032d1873996a7a8fd5bc
Mahi Pandey <mahipandey1813@gmail.com>
2026-02-24 22:42:10 +0530

    Update leetcode_0101.java

--- LeetCode/Trees/leetcode_0101.java
+++ LeetCode/Trees/leetcode_0101.java
@@ -5,3 +5,37 @@
 // Learning: Symmetry in binary trees requires mirror comparison, where recursive calls pair opposite children and combine results using logical AND to verify both structure and values
 
 // Solution
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null) return true;
+
+        return isMirror(root.left,root.right);
+    }
+
+    private boolean isMirror(TreeNode left, TreeNode right){
+        
+        if(left == null && right == null) return true;
+        if(left == null || right == null) return false;
+
+        if(left.val != right.val) return false;
+        if(left.left==right.right && left.right==right.left){
+            return true;
+        }
+        return isMirror(left.left,right.right) && isMirror(left.right,right.left);
+    }
+}
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
