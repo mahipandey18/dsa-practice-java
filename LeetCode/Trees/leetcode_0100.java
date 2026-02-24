d380764b7e91d69fd30c105fc5fea6b77ffbe6a4
Mahi Pandey <mahipandey1813@gmail.com>
2026-02-24 20:16:10 +0530

    Update leetcode_0100.java

--- LeetCode/Trees/leetcode_0100.java
+++ LeetCode/Trees/leetcode_0100.java
@@ -5,3 +5,30 @@
 // Learning: Tree comparison problems rely on matching both structure and node values using recursion, where each call verifies current nodes and combines subtree results using logical AND
 
 // Solution
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null) return true;
+        if(p == null || q == null){
+            return false;
+        }
+        if(p.val!=q.val){
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
